c73cc40484178c7a3344cb7e31951f4f904d8c5e
Jacob <jacoballen4534@gmail.com>
2020-03-11 23:39:39 +1300

    Added babel to allow es6 features. Add a prettier config to prittier plugin with eslint. Move configuration settings into a config file. Add more start scripts for running babel Expanded on the example endpoint to demonstrate paths and patch requests.

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+// Require Modules
+import express from 'express';
+
+// Middleware
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+
+// Routes
+import exampleEndpoint from './routes/exampleEndpoint';
+
+// Initialise Express and apply middleware
+const app = express();
+app.use(helmet()); // Security helper plugin that removes or changes certain headers
+app.use(cors());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(morgan('dev')); // for looging
+
+// Inject all routes
+app.use('/exampleEndpoint', exampleEndpoint);
+
+export default app;
