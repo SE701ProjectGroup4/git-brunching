049c6267c5c5247db68aa84a37d004f45d1227e6
DavidChen <David.Chen@water.co.nz>
2020-03-13 18:14:39 +1300

    Added new comments

--- frontend/src/booking/BookingEditPopupButton.js
+++ frontend/src/booking/BookingEditPopupButton.js
@@ -15,10 +15,16 @@ const BookingEditPopupButton = (props) => {
     const { IDSwitchMethod } = props;
     const [open, setOpen] = React.useState(false);
 
+    /**
+     * Opens the popup
+     */
     const handleClickOpen = () => {
         setOpen(true);
     };
 
+    /**
+     * Hides the popup
+     */
     const handleClose = () => {
         setOpen(false);
     };
@@ -37,30 +43,47 @@ export default BookingEditPopupButton;
  * The popup itself which is used to edit bookings
  */
 const PopupDialog = (props) => {
-
-    const dummyRestaurant = "KCF";
+    // Setup dummy data
+    const dummyBooking = {
+        name: "KCF",
+        tables: 1,
+        people: 4,
+        notes: "Window table please"
+    };
 
     const history = useHistory();
     const [isInput, changeInput] = React.useState(true)
     const { onClose, open, IDSwitchMethod} = props;
 
+    /**
+     * Closes the popup
+     */
     const handleClosePopup = () => {
         onClose();
         // Wait for animation to finish
         resetPopup();
     };
 
+    /**
+     * Switches popup to show booking details
+     */
     const handleChangeToBookingDetails = () => {
         changeInput(false);
 
     };
 
+    /**
+     * Resets popup to default state
+     */
     const resetPopup = () => {
         changeInput(true);
     };
 
+    /**
+     * Switches page to the specified restaurant booking page
+     */
     const handleEditBooking = () => {
-        IDSwitchMethod(dummyRestaurant);
+        IDSwitchMethod(dummyBooking.name);
         changePath("/booking", history);
     };
 
@@ -83,7 +106,10 @@ const PopupDialog = (props) => {
         <Dialog onClose={handleClosePopup} aria-labelledby="simple-dialog-title" open={open}>
             <div>
                 <p>
-                    You have booked 1 table for 4 people at KCF
+                    You have booked <b>{dummyBooking.tables}</b> table(s) for <b>{dummyBooking.people}</b> people at
+                     "<b>{dummyBooking.name}</b>"!!<br />
+                    You have given the extra notes:<br />
+                    <b>{dummyBooking.notes}</b>
                 </p>
             </div>
             <Button variant="outlined" color="primary" fullWidth={false} onClick={handleEditBooking}>
