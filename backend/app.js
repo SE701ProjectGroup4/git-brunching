eeac6ad20fc7d8d988d8a4f07808820702f071c1
william-shin-387 <william-shin-387@gmail.com>
2020-03-13 16:38:37 +1300

    fix route import in app.js

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 
 // Routes
-import exampleEndpoint from './routes/exampleEndpoint';
-import restaurant from '.routes/restaurant';
+import restaurant from './routes/restaurant';
 
 // Initialise Express and apply middleware
 const app = express();
