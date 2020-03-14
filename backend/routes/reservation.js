599930c6c91b1084e8e962b6e95d0eb6f366e5b8
jacoballen4534 <jacoballen4534@gmail.com>
2020-03-14 19:34:14 +1300

    Get all resurvations for a resteraunt.

--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -30,4 +30,18 @@ router.get('/single', (req, res) => {
   );
 });
 
+// Retrieve all reservation from a single restaurant. Used by restaurant manager to see all reservations.
+router.get('/all', (req, res) => {
+  connection.query(
+    `SELECT ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID FROM RESERVATION;`,
+    (error, results) => {
+      if (error) {
+        res.status(400).json({ error });
+        return;
+      }
+      res.json({ result: results });
+    }
+  );
+});
+
 export default router;
