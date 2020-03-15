d6feb723831c432e1fa3c3d04e648ada490a0f51
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2020-03-16 09:52:58 +1300

    Update individual user fields

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import connection from '../database';
+import config from '../config/config';
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -32,17 +33,16 @@ router.post('/', async (req, res) => {
 router.put('/', async (req, res) => {
   const { body } = req;
 
-  if (!body.reservationID || !body.firstName || !body.lastName || !body.phone || !body.email) {
+  if (!body.reservationID || (!body.firstName && !body.lastName && !body.phone && !body.email)) {
     res
       .status(400)
-      .json({ error: '/user PUT endpoint needs firstName, lastName, phone, email, and reservationID body params' });
+      .json({ error: '/user PUT endpoint needs firstName, lastName, phone, OR email, AND reservationID body params' });
     return;
   }
 
-  const { error } = await connection.asyncQuery(
-    `UPDATE USER, RESERVATION SET USER.FirstName = ?, USER.LastName = ?, USER.Phone = ?, USER.Email = ? 
-                      WHERE RESERVATION.UserID = USER.ID AND RESERVATION.ID = ?`,
-    [body.firstName, body.lastName, body.phone, body.email, body.reservationID]
+  const { error, result } = await connection.asyncQuery(
+    'SELECT * FROM USER JOIN RESERVATION ON USER.ID = RESERVATION.UserID WHERE RESERVATION.ID = ?',
+    [body.reservationID]
   );
 
   if (error) {
@@ -50,7 +50,64 @@ router.put('/', async (req, res) => {
     return;
   }
 
-  res.json({ result: 'Updated user' });
+  let newFirstName;
+  let newLastName;
+  let newPhone;
+  let newEmail;
+
+  // For each parameter, check if it has been provided in endpoint, otherwise use previous value.
+  // The mock SQL doesn't return a proper result, so a default value is assigned when testing
+  if (body.firstName) {
+    newFirstName = body.firstName;
+  } else if (config.mock) {
+    newFirstName = 'test';
+  } else {
+    newFirstName = result[0].FirstName;
+  }
+
+  if (body.lastName) {
+    newLastName = body.lastName;
+  } else if (config.mock) {
+    newLastName = 'user';
+  } else {
+    newLastName = result[0].LastName;
+  }
+
+  if (body.phone) {
+    newPhone = body.phone;
+  } else if (config.mock) {
+    newPhone = '091234567';
+  } else {
+    newPhone = result[0].Phone;
+  }
+
+  if (body.email) {
+    newEmail = body.email;
+  } else if (config.mock) {
+    newEmail = 'testuser@email.com';
+  } else {
+    newEmail = result[0].Email;
+  }
+
+  const { err } = await connection.asyncQuery(
+    `UPDATE USER, RESERVATION SET USER.FirstName = ?, USER.LastName = ?, USER.Phone = ?, USER.Email = ? 
+                      WHERE RESERVATION.UserID = USER.ID AND RESERVATION.ID = ?`,
+    [newFirstName, newLastName, newPhone, newEmail, body.reservationID]
+  );
+
+  if (err) {
+    res.status(400).json({ err });
+    return;
+  }
+
+  // The mock SQL doesn't return a proper result, so a default value is assigned when testing
+  let userID;
+  if (config.mock) {
+    userID = '303';
+  } else {
+    userID = result[0].ID;
+  }
+  res.json({ result: 'Updated user', userID });
 });
 
 export default router;
--- backend/tests/user/testUpdateUser.js
+++ backend/tests/user/testUpdateUser.js
@@ -31,75 +31,21 @@ describe('PUT /user/', () => {
     }
   });
 
-  it('2. should return expected error when the firstName parameter is missing.', async () => {
+  it('2. should return expected error when all parameters other than reservationID are missing.', async () => {
     let response = await chai
       .request(`${config.listen.address}:${config.listen.port}/user`)
       .put('/')
       .send({
-        lastName: 'user',
-        phone: '091234567',
-        email: 'testuser@email.com',
         reservationID: '300'
       });
 
     assert.isObject(response, 'Expected the response to be an object');
     assert.deepEqual(response.body, {
-      error: '/user PUT endpoint needs firstName, lastName, phone, email, and reservationID body params'
+      error: '/user PUT endpoint needs firstName, lastName, phone, OR email, AND reservationID body params'
     });
   });
 
-  it('3. should return expected error when the lastName parameter is missing.', async () => {
-    let response = await chai
-      .request(`${config.listen.address}:${config.listen.port}/user`)
-      .put('/')
-      .send({
-        firstName: 'test',
-        phone: '091234567',
-        email: 'testuser@email.com',
-        reservationID: '300'
-      });
-
-    assert.isObject(response, 'Expected the response to be an object');
-    assert.deepEqual(response.body, {
-      error: '/user PUT endpoint needs firstName, lastName, phone, email, and reservationID body params'
-    });
-  });
-
-  it('4. should return expected error when the phone parameter is missing.', async () => {
-    let response = await chai
-      .request(`${config.listen.address}:${config.listen.port}/user`)
-      .put('/')
-      .send({
-        firstName: 'test',
-        lastName: 'user',
-        email: 'testuser@email.com',
-        reservationID: '300'
-      });
-
-    assert.isObject(response, 'Expected the response to be an object');
-    assert.deepEqual(response.body, {
-      error: '/user PUT endpoint needs firstName, lastName, phone, email, and reservationID body params'
-    });
-  });
-
-  it('5. should return expected error when the email parameter is missing.', async () => {
-    let response = await chai
-      .request(`${config.listen.address}:${config.listen.port}/user`)
-      .put('/')
-      .send({
-        firstName: 'test',
-        lastName: 'user',
-        phone: '091234567',
-        reservationID: '300'
-      });
-
-    assert.isObject(response, 'Expected the response to be an object');
-    assert.deepEqual(response.body, {
-      error: '/user PUT endpoint needs firstName, lastName, phone, email, and reservationID body params'
-    });
-  });
-
-  it('6. should return expected error when the reservationID parameter is missing.', async () => {
+  it('3. should return expected error when the reservationID parameter is missing.', async () => {
     let response = await chai
       .request(`${config.listen.address}:${config.listen.port}/user`)
       .put('/')
@@ -112,7 +58,75 @@ describe('PUT /user/', () => {
 
     assert.isObject(response, 'Expected the response to be an object');
     assert.deepEqual(response.body, {
-      error: '/user PUT endpoint needs firstName, lastName, phone, email, and reservationID body params'
+      error: '/user PUT endpoint needs firstName, lastName, phone, OR email, AND reservationID body params'
     });
+  });
+
+  it('4. should successfully update if only reservationID and firstName are provided.', async () => {
+    let response = await chai
+      .request(`${config.listen.address}:${config.listen.port}/user`)
+      .put('/')
+      .send({
+        firstName: 'test',
+        reservationID: '300'
+      });
+
+    const { userID, result } = response.body;
+    assert.strictEqual(result, 'Updated user');
+
+    if (!config.mock) {
+      assert.isNumber(userID, 'Expected userID to be a number');
+    }
+  });
+
+  it('5. should successfully update if only reservationID and lastName are provided.', async () => {
+    let response = await chai
+      .request(`${config.listen.address}:${config.listen.port}/user`)
+      .put('/')
+      .send({
+        lastName: 'user',
+        reservationID: '300'
+      });
+
+    const { userID, result } = response.body;
+    assert.strictEqual(result, 'Updated user');
+
+    if (!config.mock) {
+      assert.isNumber(userID, 'Expected userID to be a number');
+    }
+  });
+
+  it('6. should successfully update if only reservationID and phone are provided.', async () => {
+    let response = await chai
+      .request(`${config.listen.address}:${config.listen.port}/user`)
+      .put('/')
+      .send({
+        phone: '091234567',
+        reservationID: '300'
+      });
+
+    const { userID, result } = response.body;
+    assert.strictEqual(result, 'Updated user');
+
+    if (!config.mock) {
+      assert.isNumber(userID, 'Expected userID to be a number');
+    }
+  });
+
+  it('7. should successfully update if only reservationID and email are provided.', async () => {
+    let response = await chai
+      .request(`${config.listen.address}:${config.listen.port}/user`)
+      .put('/')
+      .send({
+        email: 'testuser@email.com',
+        reservationID: '300'
+      });
+
+    const { userID, result } = response.body;
+    assert.strictEqual(result, 'Updated user');
+
+    if (!config.mock) {
+      assert.isNumber(userID, 'Expected userID to be a number');
+    }
   });
 });
