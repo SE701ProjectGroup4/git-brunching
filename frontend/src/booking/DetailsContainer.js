07e02a307b140a4f1001356a0704c940487fa170
Aditya Krishnan <adityakrishnan99@gmail.com>
2020-03-15 13:43:48 +1300

    styling improved

--- frontend/src/booking/DetailsContainer.js
+++ frontend/src/booking/DetailsContainer.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import TextField from "@material-ui/core/TextField";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import Card from '@material-ui/core/Card';
 import style from "./BookingPage.module.css";
 import changePath from "../general/helperFunctions";
 import messages from "../general/textHolder";
 
 const detailMessages = messages.details;
-const DetailsContainer = () => {
+const DetailsContainer = (props) => {
   const history = useHistory();
   const [name, handleName] = useState("");
   const [email, handleEmail] = useState("");
@@ -23,61 +24,59 @@ const DetailsContainer = () => {
     changePath("/", history);
   };
 
+  //where information user enters is stored.
+  const information = {
+    name: '',
+    email: '',
+    phone:''
+  }
+
+  const handleName = (e) => {
+    
+  };
+
+  const handleEmail = () => {
+    
+  };
+
+  const handlePhoneNumber = () => {
+    
+  };
+
+
+
   return (
     <div className={style.contentContainer}>
       {/* Input fields go here */}
       <div className={style.inputContainer}>
-        <Card className={style.detailsCard}>
-          <h1>Enter Your Details</h1>
-          {
-          /**
-           * Form for inputting personal details.
-           */
-           }
+        <Card className = {style.detailsCard}>
+          <h1>Enter Your Details</h1>  {/* put into textholder.js later*/}
           <form>
             <div className="form-group">
-              <label className={style.formlabel}>Name</label>
-              <TextField
-                type="text"
-                name="username"
-                className="form-value"
-                onChange={
-                (e) => { handleName(e.target.value); }
-              }
-              />
+              <label className = {style.formlabel}>Name</label>
+              <TextField type = "text" name = "username" className="form-value"/>
             </div>
             <div className="form-group">
-              <label className={style.formlabel}>Phone Number</label>
-              <TextField
-                type="text"
-                name="phonenumber"
-                className="form-value"
-                onChange={
-                (e) => { handlePhoneNumber(e.target.value); }
-                }
-              />
+              <label className = {style.formlabel}>Phone Number</label>
+              <TextField type = "text" name = "phonenumber" className="form-value" />
             </div>
             <div className="form-group">
-              <label className={style.formlabel}>Email</label>
-              <TextField
-                type="text"
-                name="email"
-                className="form-value"
-                onChange={
-                  (e) => { handleEmail(e.target.value); }
-                }
-              />
+              <label className = {style.formlabel}>Email</label>
+              <TextField type = "text" name = "email" className="form-value" />
             </div>
             <div className="form-group">
-              <label className={style.formlabel}>Notes</label>
-              <TextField />
+              <label className = {style.formlabel}>Notes</label>
+              <TextField></TextField>
             </div>
             <div className={style.buttonContainer}>
-              <BottomNavigation>
-                <button className={style.changePageButton} variant="contained" onClick={handlePrevious}>{detailMessages.buttonBackText}</button>
-                <button className={style.changePageButton} type="submit" variant="contained" onClick={handleNext}>{detailMessages.buttonNextText}</button>
-              </BottomNavigation>
-            </div>
+            <BottomNavigation>
+              <button className ={style.changePageButton} variant="contained" onClick={() => changePath("/", history)} >{detailMessages.buttonBackText}</button>
+              <button className ={style.changePageButton} type ="submit" variant="contained" onClick={() => changePath("/confirmation", history)} >{detailMessages.buttonNextText}</button>
+            </BottomNavigation>
+            </div>            
+
+         
+          
           </form>
         </Card>
       </div>
