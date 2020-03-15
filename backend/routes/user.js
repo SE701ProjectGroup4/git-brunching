823adfc3b9ff9b1dc9d032f7bad1dd80e834a396
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2020-03-15 13:40:22 +1300

    fixed insert sql query

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,6 +6,7 @@ import config from '../config/config';
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 
+<<<<<<< HEAD
 // Add new user to database. Used when adding a new reservation, so that a user can identify themself.
 router.post('/', async (req, res) => {
   const { body } = req;
@@ -91,3 +92,41 @@ router.put('/', async (req, res) => {
 });
 
 export default router;
+=======
+router.post('/', (req, res) => {
+    const { body } = req;
+  
+    if (!body.firstName || !body.lastName || !body.phone || !body.email) {
+        res.status(400).json({ error: '/user POST endpoint needs firstName, lastName, phone, and email body params' });
+         return;
+       }
+  
+    connection.query(`INSERT INTO USER(FirstName, LastName, Phone, Email) VALUES (?, ?, ?, ?);`, [body.firstName, body.lastName, body.phone, body.email], error => {
+        if (error) {
+          res.status(400).json({ error });
+          return;
+       }
+       res.json('user created');
+     });
+   });
+
+router.put('/', (req, res) => {
+    const { body } = req;
+  
+    if (!body.reservationID || !body.firstName || !body.lastName || !body.phone || !body.email) {
+        res.status(400).json({ error: '/user PUT endpoint needs reservationID, firstName, lastName, phone, and email body params' });
+         return;
+       }
+  
+    connection.query(`UPDATE USER, RESERVATION SET USER.FirstName = ?, USER.LastName = ?, USER.Phone = ?, USER.Email = ? 
+                      WHERE RESERVATION.ID = ?`, [body.firstName, body.lastName, body.phone, body.email, body.reservationID], error => {
+        if (error) {
+          res.status(400).json({ error });
+          return;
+       }
+       res.json('user updated');
+     });
+   });
+
+export default router;
+>>>>>>> fixed insert sql query
