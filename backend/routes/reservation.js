ad5a720f4e27e5adb01e75c8399a92558e502d88
jacoballen4534 <jacoballen4534@gmail.com>
2020-03-15 14:51:48 +1300

    Convert to async querys. Validate resurvation times before deleting

--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 
 // Retrieve a single reservation for a user to check their reservation.
-router.get('/single', (req, res) => {
+router.get('/single', async (req, res) => {
   const { reservationID } = req.query;
 
   if (!reservationID) {
@@ -15,23 +15,22 @@ router.get('/single', (req, res) => {
     return;
   }
 
-  connection.query(
-    `SELECT ` +
-      `ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID FROM RESERVATION ` +
-      `WHERE ID = ? `,
-    [reservationID],
-    (error, results) => {
-      if (error) {
-        res.status(400).json({ error });
-        return;
-      }
-      res.json({ result: results });
-    }
+  const { error, result } = await connection.asyncQuery(
+    'SELECT ' +
+      'ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID FROM RESERVATION ' +
+      'WHERE ID = ? ',
+    [reservationID]
   );
+
+  if (error) {
+    res.status(400).json({ error });
+    return;
+  }
+  res.json({ result });
 });
 
 // Retrieve all reservation from a single restaurant. Used by restaurant manager to see all reservations.
-router.get('/all', (req, res) => {
+router.get('/all', async (req, res) => {
   const { restaurantID } = req.query;
 
   if (!restaurantID) {
@@ -39,21 +38,22 @@ router.get('/all', (req, res) => {
     return;
   }
 
-  connection.query(
-    `SELECT ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID FROM RESERVATION WHERE RestaurantID = ?;`,
-    [restaurantID],
-    (error, results) => {
-      if (error) {
-        res.status(400).json({ error });
-        return;
-      }
-      res.json({ result: results });
-    }
+  const { error, result } = await connection.asyncQuery(
+    'SELECT ' +
+      'ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID FROM RESERVATION ' +
+      'WHERE RestaurantID = ?;',
+    [restaurantID]
   );
+
+  if (error) {
+    res.status(400).json({ error });
+    return;
+  }
+  res.json({ result });
 });
 
 // Add a new reservation when a user wants to book a table.
-router.post('/single', (req, res) => {
+router.post('/single', async (req, res) => {
   // Generate a random number capped at 2147483647 as this is the largest number the database can hold.
   const reservationID = Math.floor(Math.random() * 2147483647);
   const { date, time, notes, numberOfGuests, tableID, restaurantID, userID } = req.body;
@@ -61,27 +61,27 @@ router.post('/single', (req, res) => {
   if (!date || !time || !numberOfGuests || !tableID || !restaurantID || !userID) {
     res.status(400).json({
       error:
-        'reservation/single POST endpoint needs: date, time, numberOfGuests, tableID, restaurantID and userID body params',
+        'reservation/single POST endpoint needs: date, time, numberOfGuests, tableID, restaurantID and userID body params'
     });
     return;
   }
 
-  connection.query(
-    `INSERT INTO RESERVATION (ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID) ` +
-      `VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
-    [reservationID, date, time, notes, numberOfGuests, tableID, restaurantID, userID],
-    error => {
-      if (error) {
-        res.status(400).json({ error });
-        return;
-      }
-      res.json({ result: 'Added single reservation', reservationID });
-    }
+  const { error } = await connection.asyncQuery(
+    'INSERT ' +
+      'INTO RESERVATION (ID, Date, Time, Notes, NumberOfGuests, TableID, RestaurantID, UserID) ' +
+      'VALUES (?, ?, ?, ?, ?, ?, ?, ?);',
+    [reservationID, date, time, notes, numberOfGuests, tableID, restaurantID, userID]
   );
+
+  if (error) {
+    res.status(400).json({ error });
+    return;
+  }
+  res.json({ result: 'Added single reservation', reservationID });
 });
 
 // Delete a reservation when a customer want to cancel a booking.
-router.delete('/single', (req, res) => {
+router.delete('/single', async (req, res) => {
   const { reservationID } = req.query;
 
   if (!reservationID) {
@@ -89,13 +89,52 @@ router.delete('/single', (req, res) => {
     return;
   }
 
-  connection.query(`DELETE FROM RESTAURANT WHERE ID=?;`, [reservationID], error => {
-    if (error) {
-      res.status(400).json({ error });
-      return;
-    }
-    res.json({ result: 'Deleted reservation' });
-  });
+  const {
+    error: timeError,
+    result: row
+  } = await connection.asyncQuery('SELECT DATE, TIME FROM RESERVATION WHERE ID = ?;', [reservationID]);
+
+  if (timeError) {
+    res.status(400).json({ error: 'Could not retrieve this reservation' });
+    return;
+  }
+
+  const timeRow = JSON.parse(JSON.stringify(row)); // Convert SQL row into readable array.
+
+  if (!timeRow || !Array.isArray(timeRow) || timeRow.length !== 1) {
+    res.status(400).json({ error: 'This reservation does not exist' });
+    return;
+  }
+
+  const reservationTime = (timeRow[0] || {}).TIME;
+  const [hour, min, sec] = reservationTime.split(':').map(token => Number(token));
+
+  const reservationDateTime = new Date((timeRow[0] || {}).DATE);
+  reservationDateTime.setHours(reservationDateTime.getHours() + hour);
+  reservationDateTime.setMinutes(reservationDateTime.getMinutes() + min);
+  reservationDateTime.setSeconds(reservationDateTime.getSeconds() + sec);
+  const currentDateTime = new Date();
+
+  // Ensure the reservation has not already passed
+  if (reservationDateTime < currentDateTime) {
+    res.status(400).json({ error: 'This reservation has already passed' });
+    return;
+  }
+
+  // Ensure the reservation is more than 1 hour away
+  reservationDateTime.setHours(reservationDateTime.getHours() - 1);
+
+  if (reservationDateTime < currentDateTime) {
+    res.status(400).json({ error: 'Can only cancel reservations more than 1 hour in advance' });
+    return;
+  }
+
+  const { error } = await connection.asyncQuery('DELETE FROM RESERVATION WHERE ID=?;', [reservationID]);
+  if (error) {
+    res.status(400).json({ error });
+    return;
+  }
+  res.json({ result: 'Deleted reservation' });
 });
 
 export default router;
