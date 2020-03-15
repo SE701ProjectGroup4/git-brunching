7faa5d3cd994631c5a5b2f9a88ba1d4eae229114
Aditya Krishnan <adityakrishnan99@gmail.com>
2020-03-15 19:23:46 +1300

    new branch created because error in previous one

--- frontend/src/booking/DetailsContainer.js
+++ frontend/src/booking/DetailsContainer.js
@@ -24,27 +24,6 @@ const DetailsContainer = (props) => {
     changePath("/", history);
   };
 
-  //where information user enters is stored.
-  const information = {
-    name: '',
-    email: '',
-    phone:''
-  }
-
-  const handleName = (e) => {
-    
-  };
-
-  const handleEmail = () => {
-    
-  };
-
-  const handlePhoneNumber = () => {
-    
-  };
-
-
-
   return (
     <div className={style.contentContainer}>
       {/* Input fields go here */}
