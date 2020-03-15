68e073dec146be1ab5a3f170e0309ed684823e73
Aditya Krishnan <adityakrishnan99@gmail.com>
2020-03-15 13:43:48 +1300

    styling improved

--- frontend/src/booking/DetailsContainer.js
+++ frontend/src/booking/DetailsContainer.js
@@ -24,6 +24,27 @@ const DetailsContainer = (props) => {
     changePath("/", history);
   };
 
+  //where information user enters is stored.
+  const information = {
+    name: '',
+    email: '',
+    phone:''
+  }
+
+  const handleName = (e) => {
+    
+  };
+
+  const handleEmail = () => {
+    
+  };
+
+  const handlePhoneNumber = () => {
+    
+  };
+
+
+
   return (
     <div className={style.contentContainer}>
       {/* Input fields go here */}
