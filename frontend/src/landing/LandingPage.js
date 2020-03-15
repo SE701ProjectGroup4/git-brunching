0a17ba6accebfcb63ad42e5abe20dc56183bd0aa
Kwu849 <kwu849@aucklanduni.ac.nz>
2020-03-15 16:07:46 +1300

    completed rebase merge

--- frontend/src/landing/LandingPage.js
+++ frontend/src/landing/LandingPage.js
@@ -1,47 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router";
 import style from "./LandingPage.module.css";
+import changePath from "../general/helperFunctions";
 import messages from "../general/textHolder";
-<<<<<<< HEAD
 import BookingEditPopupButton from "../booking/BookingEditPopupButton";
-
-=======
->>>>>>> created restaurant tile component
 import RestaurantTile from "./RestaurantTile";
 
 const landingText = messages.landingPage;
 
 const LandingPage = (props) => {
   const { setRestaurant } = props;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   const history = useHistory();
 
   const toBooking = (restaurant) => {
     changePath("/booking", history);
     setRestaurant(restaurant);
   };
-
-  const fakeData = [
-    {
-      name: "NANDOZ",
-      times: [],
-    },
-    {
-      name: "KCF",
-      times: [],
-    },
-  ];
-
-=======
-  
->>>>>>> added styling to restaurant tile
-=======
-
->>>>>>> fixed code formatting
-=======
-
->>>>>>> fixed code formatting
   return (
     <div className={style.landingPageContainer}>
       <div className={style.header}>
@@ -51,11 +25,7 @@ const LandingPage = (props) => {
         {landingText.search}
         <BookingEditPopupButton IDSwitchMethod={toBooking} />
       </div>
-<<<<<<< HEAD
       <RestaurantTile setRestaurant={setRestaurant} />
-=======
-      <RestaurantTile setRestaurant={setRestaurant}/>
->>>>>>> created restaurant tile component
       {/* Probably make it it's own component */}
       <div className={style.footer}>
         {landingText.footer}
