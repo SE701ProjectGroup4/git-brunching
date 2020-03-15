7b4455e5355332d0f6d9e45565112b6708e2a137
jacoballen4534 <jacoballen4534@gmail.com>
2020-03-15 14:48:50 +1300

    Update linting settings and fix lint errors

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 export default {
   listen: {
     address: '127.0.0.1',
-    port: 3001,
+    port: 3001
   },
-  mock: false,
+  mock: false
 };
--- backend/sqlConnectionResolver.js
+++ backend/sqlConnectionResolver.js
@@ -2,6 +2,7 @@ import mysql from 'mysql';
 import config from './config/config';
 import mocksql from './tests/mockSQL';
 
+// eslint-disable-next-line import/no-mutable-exports
 let sqlConnection;
 if (config.mock) {
   sqlConnection = mocksql;
