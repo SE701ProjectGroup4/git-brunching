ba1ae1862546b89d8115a4a44cd003fb5bc26355
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2020-03-15 15:43:23 +1300

    Formatting with prettier, mount user on app

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,8 +6,39 @@ import config from '../config/config';
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-// Add new user to database. Used when adding a new reservation, so that a user can identify themself.
+/**
+ * @swagger
+ *
+ * /user:
+ *   post:
+ *     description: Adds a user object to the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: firstName
+ *         description: User's given name
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: lastName
+ *         description: User's surname
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: phone
+ *         description: User's telephone number
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         description: User's email address
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully added user to database
+ */
 router.post('/', async (req, res) => {
   const { body } = req;
 
@@ -30,7 +61,44 @@ router.post('/', async (req, res) => {
   res.json({ result: 'Added user', userID });
 });
 
-// Update user in database.
+/**
+ * @swagger
+ *
+ * /user:
+ *   put:
+ *     description: Update a user in the database, identified by the booking that user has made. At least one parameter out of firstName, lastName, phone, or email must be entered.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: firstName
+ *         description: User's new given name
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: lastName
+ *         description: User's new surname
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: phone
+ *         description: User's new telephone number
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: email
+ *         description: User's new email address
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: reservationID
+ *         description: ID of the reservation that the user has made. Primary key of reservation database table
+ *         in: formData
+ *         required: true
+ *         type: string 
+ *     responses:
+ *       200:
+ *         description: Successfully updated user in database
+ */
 router.put('/', async (req, res) => {
   const { body } = req;
 
@@ -92,41 +160,3 @@ router.put('/', async (req, res) => {
 });
 
 export default router;
-=======
-router.post('/', (req, res) => {
-    const { body } = req;
-  
-    if (!body.firstName || !body.lastName || !body.phone || !body.email) {
-        res.status(400).json({ error: '/user POST endpoint needs firstName, lastName, phone, and email body params' });
-         return;
-       }
-  
-    connection.query(`INSERT INTO USER(FirstName, LastName, Phone, Email) VALUES (?, ?, ?, ?);`, [body.firstName, body.lastName, body.phone, body.email], error => {
-        if (error) {
-          res.status(400).json({ error });
-          return;
-       }
-       res.json('user created');
-     });
-   });
-
-router.put('/', (req, res) => {
-    const { body } = req;
-  
-    if (!body.reservationID || !body.firstName || !body.lastName || !body.phone || !body.email) {
-        res.status(400).json({ error: '/user PUT endpoint needs reservationID, firstName, lastName, phone, and email body params' });
-         return;
-       }
-  
-    connection.query(`UPDATE USER, RESERVATION SET USER.FirstName = ?, USER.LastName = ?, USER.Phone = ?, USER.Email = ? 
-                      WHERE RESERVATION.UserID = USER.ID AND RESERVATION.ID = ?`, [body.firstName, body.lastName, body.phone, body.email, body.reservationID], error => {
-        if (error) {
-          res.status(400).json({ error });
-          return;
-       }
-       res.json('user updated');
-     });
-   });
-
-export default router;
->>>>>>> fixed insert sql query
