cdb5839dc250810bdedb8c7ac6ccab648950bb7d
nidhineshnand <nnidhinesh@gmail.com>
2020-03-16 17:46:53 +1300

    Fixed linting issues

--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -357,11 +357,10 @@ router.delete('/:reservationID', async (req, res) => {
   res.json({ result: 'Deleted reservation' });
 });
 
-//This call gets the list of available tables for reservation
+// This call gets the list of available tables for reservation
 router.get('/available', async (req, res) => {
   const { date, time, numberOfGuests, restaurantID } = req.body;
 
-  console.log(req.body)
   if (!date || !time || !numberOfGuests || !restaurantID) {
     res.status(400).json({
       error:
@@ -371,22 +370,22 @@ router.get('/available', async (req, res) => {
   }
 
   const { error, result } = await connection.asyncQuery(
-    "SELECT * " +
-    "FROM restaurant_db.TABLE t " +
-    "WHERE t.RestaurantID = ? AND t.maxGuests >= ? AND NOT EXISTS ( SELECT * " +
-                                                  "FROM RESERVATION r " +
-                                                  "WHERE t.RestaurantID = r.RestaurantID AND " +
-                                                  "t.ID = r.TableID AND " +
-                                                  "r.Date = ? AND " +
-                                                  "r.Time = ? );" ,
-    [restaurantID, numberOfGuests, date, time])
+    'SELECT * ' +
+    'FROM restaurant_db.TABLE t ' +
+    'WHERE t.RestaurantID = ? AND t.maxGuests >= ? AND NOT EXISTS ( SELECT * ' +
+                                                  'FROM RESERVATION r ' +
+                                                  'WHERE t.RestaurantID = r.RestaurantID AND ' +
+                                                  't.ID = r.TableID AND ' +
+                                                  'r.Date = ? AND ' +
+                                                  'r.Time = ? );',
+    [restaurantID, numberOfGuests, date, time]
+  );
 
-    if (error) {
-      res.status(400).json({ error });
-      return;
-    }
-    res.json({ result });
-
+  if (error) {
+    res.status(400).json({ error });
+    return;
+  }
+  res.json({ result });
 });
 
 
