4cd722e99b7ab3b8014be3036451265ffd0413c8
ljcnz <liam@ljc.nz>
2020-03-17 03:39:18 +1300

    refactored restaurant endpoints

--- backend/routes/restaurant.js
+++ backend/routes/restaurant.js
@@ -18,22 +18,22 @@ router.use(bodyParser.urlencoded({ extended: true }));
  *     parameters:
  *       - name: restaurantID
  *         description: Primary Key of Restaurant database table
- *         in: query
+ *         in: path
  *         required: true
  *         type: integer
  *     responses:
  *       200:
  *         description: Returns restaurant object
  */
-router.get('/', (req, res) => {
-  const input = req.query;
+router.get('/:restaurantID', async (req, res) => {
+  const { restaurantID } = req.params;
 
-  if (!input.restaurantID) {
-    res.status(400).json({ error: '/restaurant GET endpoint needs a restaurantID query param' });
+  if (!restaurantID) {
+    res.status(400).json({ error: 'GET /restaurant/{id} invocation error: {id} must be an int' });
     return;
   }
 
-  connection.query('SELECT * FROM RESTAURANT WHERE ID = ? ', [req.query.restaurantID], (error, results) => {
+  connection.query('SELECT * FROM RESTAURANT WHERE ID = ? ', [restaurantID], (error, results) => {
     if (error) {
       res.status(400).json({ error });
       return;
@@ -43,15 +43,15 @@ router.get('/', (req, res) => {
 });
 
 // Gets specific restaurant opening hours, used by front end to build restaurant opening hours for end user.
-router.get('/openhours', (req, res) => {
-  const input = req.query;
-  if (!input.restaurantID) {
-    res.status(400).json({ error: '/restaurant/openhours GET endpoint needs a restaurantID query param' });
+router.get('/:restaurantID/openhours', async (req, res) => {
+  const { restaurantID } = req.params;
+  if (!restaurantID) {
+    res.status(400).json({ error: 'GET /restaurant/{id}/openhours invocation error: {id} must be an int' });
     return;
   }
 
   connection.query(
-    'SELECT DayOfWeek, OpenTime, CloseTime from HOURS WHERE RestaurantID = ?;', [input.restaurantID],
+    'SELECT DayOfWeek, OpenTime, CloseTime from HOURS WHERE RestaurantID = ?;', [restaurantID],
     (error, results) => {
       if (error) {
         res.status(400).json({ error });
@@ -63,7 +63,7 @@ router.get('/openhours', (req, res) => {
 });
 
 // Gets all restaurants ID and name from database. Used to get all available restaurants by front end.
-router.get('/getall', (req, res) => {
+router.get('/', (req, res) => {
   const input = req.query;
 
   if (JSON.stringify(input) !== '{}') {
@@ -105,7 +105,7 @@ router.post('/', (req, res) => {
   const { body } = req;
 
   if (!body.name) {
-    res.status(400).json({ error: '/restaurant POST endpoint needs name body param' });
+    res.status(400).json({ error: 'POST restaurant/ invocation error: post body needs { name }' });
     return;
   }
 
@@ -129,22 +129,22 @@ router.post('/', (req, res) => {
  *     parameters:
  *       - name: restaurantID
  *         description: Primary Key of Restaurant database table
- *         in: query
+ *         in: path
  *         required: true
  *         type: integer
  *     responses:
  *       200:
  *         description: Successfully deleted restaurant to database
  */
-router.delete('/', (req, res) => {
-  const input = req.query;
+router.delete('/:restaurantID', (req, res) => {
+  const { restaurantID } = req.params;
 
-  if (!input.restaurantID) {
-    res.status(400).json({ error: '/restaurant DELETE endpoint needs a restaurantID' });
+  if (!restaurantID) {
+    res.status(400).json({ error: 'DELETE /restaurant/{id} invocation error: {id} must be an int' });
     return;
   }
 
-  connection.query('DELETE FROM RESTAURANT WHERE ID=?;', [input.restaurantID], error => {
+  connection.query('DELETE FROM RESTAURANT WHERE ID=?;', [restaurantID], error => {
     if (error) {
       res.status(400).json({ error });
       return;
