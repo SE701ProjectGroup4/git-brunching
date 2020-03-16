767ddf3e036b9ac938f33334fff314c9292f3f9a
Aditya Krishnan <adityakrishnan99@gmail.com>
2020-03-16 19:29:40 +1300

    eslitn errors

--- frontend/src/booking/DetailsContainer.js
+++ frontend/src/booking/DetailsContainer.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
-<<<<<<< HEAD
 import { connect } from "react-redux";
 import TextField from "@material-ui/core/TextField";
-=======
-import TextField from "@material-ui/core/TextField";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
->>>>>>> f2e9e6533e678ea9a2d5f116b7cb8ad75a80dde7
 import Card from "@material-ui/core/Card";
 import style from "./BookingPage.module.css";
 import changePath from "../general/helperFunctions";
@@ -18,7 +13,6 @@ const DetailsContainer = (props) => {
   const history = useHistory();
   //  functions that are used to navigate to previous and next screens
 
-<<<<<<< HEAD
   const {
     oldName,
     oldPhone,
@@ -36,22 +30,6 @@ const DetailsContainer = (props) => {
     props.onConfirmClick(name, phone, email, notes);
   };
 
-=======
-  /**
-   * TO DO
-   * const [name, handleName] = useState("");
-  const [email, handleEmail] = useState("");
-  const [phoneNumber, handlePhoneNumber] = useState("");
-   * const handleNext = () => {
-    changePath("/confirmation", history);
-    };
-
-    const handlePrevious = () => {
-      changePath("/", history);
-    };
-
-  */
->>>>>>> f2e9e6533e678ea9a2d5f116b7cb8ad75a80dde7
   return (
     <div className={style.contentContainer}>
       {/* Input fields go here */}
@@ -61,7 +39,6 @@ const DetailsContainer = (props) => {
           <form>
             <div className="form-group">
               <label className={style.formlabel}>Name</label>
-<<<<<<< HEAD
               <TextField
                 type="text"
                 name="name"
@@ -69,26 +46,19 @@ const DetailsContainer = (props) => {
                 onChange={(e) => changeName(e.target.value)}
                 className="form-value"
               />
-=======
-              <TextField type="text" name="name" className="form-value" />
->>>>>>> f2e9e6533e678ea9a2d5f116b7cb8ad75a80dde7
             </div>
             <div className="form-group">
               <label className={style.formlabel}>Phone Number</label>
               <TextField
                 type="text"
                 name="phonenumber"
-<<<<<<< HEAD
                 value={phone}
                 onChange={(e) => changePhone(e.target.value)}
-=======
->>>>>>> f2e9e6533e678ea9a2d5f116b7cb8ad75a80dde7
                 className="form-value"
               />
             </div>
             <div className="form-group">
               <label className={style.formlabel}>Email</label>
-<<<<<<< HEAD
               <TextField
                 type="text"
                 name="email"
@@ -102,39 +72,21 @@ const DetailsContainer = (props) => {
               <TextField value={notes} onChange={(e) => changeNotes(e.target.value)} />
             </div>
             <div className={style.buttonContainer}>
-=======
-              <TextField type="text" name="email" className="form-value" />
-            </div>
-            <div className="form-group">
-              <label className={style.formlabel}>Notes</label>
-              <TextField />
-            </div>
-            <div className={style.buttonContainer}>
-              <BottomNavigation>
->>>>>>> f2e9e6533e678ea9a2d5f116b7cb8ad75a80dde7
-                <button
-                  className={style.changePageButton}
-                  variant="contained"
-                  onClick={() => changePath("/", history)}
-                >
-                  {detailMessages.buttonBackText}
-                </button>
-                <button
-                  className={style.changePageButton}
-                  type="submit"
-                  variant="contained"
-<<<<<<< HEAD
-                  onClick={handleDetailsConfirmation}
-                >
-                  {detailMessages.buttonNextText}
-                </button>
-=======
-                  onClick={() => changePath("/confirmation", history)}
-                >
-                  {detailMessages.buttonNextText}
-                </button>
-              </BottomNavigation>
->>>>>>> f2e9e6533e678ea9a2d5f116b7cb8ad75a80dde7
+              <button
+                className={style.changePageButton}
+                variant="contained"
+                onClick={() => changePath("/", history)}
+              >
+                {detailMessages.buttonBackText}
+              </button>
+              <button
+                className={style.changePageButton}
+                type="submit"
+                variant="contained"
+                onClick={handleDetailsConfirmation}
+              >
+                {detailMessages.buttonNextText}
+              </button>
             </div>
           </form>
         </Card>
