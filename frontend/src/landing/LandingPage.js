b810d88f26a6317f1356b3f5a34e848b3cad0cad
Sabatr <bnguyen12152@gmail.com>
2020-03-16 13:30:19 +1300

    Testing header styles

--- frontend/src/landing/LandingPage.js
+++ frontend/src/landing/LandingPage.js
@@ -6,6 +6,7 @@ import messages from "../general/textHolder";
 import BookingEditPopupButton from "../booking/BookingEditPopupButton";
 import RestaurantTile from "./RestaurantTile";
 import changePath from "../general/helperFunctions";
+import Logo from "../general/Logo";
 
 const landingText = messages.landingPage;
 
@@ -21,13 +22,16 @@ const LandingPage = (props) => {
   return (
     <div className={style.landingPageContainer}>
       <div className={style.header}>
-        {landingText.header}
+        <div className={style.logo}><Logo /></div>
+        <p className={style.title}>GIT BRUNCHING</p>
       </div>
-      <div className={style.searchContainer}>
-        <TextField id="standard-search" label="Search field" type="search" />
-        <BookingEditPopupButton IDSwitchMethod={toBooking} />
+      <div className={style.content}>
+        <div className={style.searchContainer}>
+          <TextField id="standard-search" label="Search field" type="search" />
+          <BookingEditPopupButton IDSwitchMethod={toBooking} />
+        </div>
+        <RestaurantTile setRestaurant={setRestaurant} />
       </div>
-      <RestaurantTile setRestaurant={setRestaurant} />
       {/* Probably make it it's own component */}
       <div className={style.footer}>
         {landingText.footer}
