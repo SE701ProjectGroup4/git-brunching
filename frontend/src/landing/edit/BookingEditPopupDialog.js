758859b0f5096038f1adaeabdeba9efd3d52688b
Sabatr <bnguyen12152@gmail.com>
2020-03-18 00:56:46 +1300

    Fixed null pointer issue

--- frontend/src/landing/edit/BookingEditPopupDialog.js
+++ frontend/src/landing/edit/BookingEditPopupDialog.js
@@ -70,7 +70,7 @@ const BookingEditPopupDialog = (props) => {
         getUserById(data.UserID).then((res) => {
           const userData = res.result[0];
           getRestaurantByID(data.RestaurantID).then((restaurant) => {
-            const restaurantData = restaurant.result[0];
+            const restaurantData = restaurant[0];
             // select({ID: data.userID, Name: "KCF"})
             select({ ID: data.userID, Name: restaurantData.Name });
             addTime(data.Date, data.NumberOfGuests, data.Time);
