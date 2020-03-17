f459900a374865a61e770fe8b8acf40a77ba2d43
nidhineshnand <nnidhinesh@gmail.com>
2020-03-17 13:19:35 +1300

    Changed api endpoint to post because a block of data is required from form submission

--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -360,37 +360,37 @@ router.delete('/:reservationID', async (req, res) => {
 /**
  * @swagger
  *
- * /resesrvation/available:
- *   get:
+ * /reservation/available:
+ *   post:
  *     description: Get a list of tables that are free for booking
  *     produces:
  *       - application/json
  *     parameters:
  *       - name: restaurantID
  *         description: Primary Key of Restaurant database table
- *         in: body
+ *         in: formData
  *         required: true
  *         type: integer
  *       - name: numberOfGuests
  *         description: The number of guests that the table is being booked for
- *         in: body
+ *         in: formData
  *         required: true
  *         type: integer
  *       - name: time
  *         description: The start time of the booking
- *         in: body
+ *         in: formData
  *         required: true
  *         type: string
  *       - name: date
  *         description: Date for when the booking is made
- *         in: body
+ *         in: formData
  *         required: true
  *         type: string
  *     responses:
  *        200:
  *         description: Returns the list of table ID's that are free for that restaurant
  */
-router.get('/available', async (req, res) => {
+router.post('/available', async (req, res) => {
   const { date, time, numberOfGuests, restaurantID } = req.body;
 
   if (!date || !time || !numberOfGuests || !restaurantID) {
