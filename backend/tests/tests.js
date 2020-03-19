2fe736a8aa9cf1a65d7f9e4402dd4bd44760d4e3
jacoballen4534 <jacoballen4534@gmail.com>
2020-03-19 20:18:55 +1300

    Prevent users from running tests on real database.

--- backend/tests/tests.js
+++ backend/tests/tests.js
@@ -1,3 +1,11 @@
+import config from '../config/config';
+
+if (!config.mock) {
+  throw '\n\n   Tests should only be run on the mock database. Please set the mock flag in the config to true.\n \
+  If you know what you are doing and wish to run this on a real database, remove this check.\n\n';
+}
+
+// List all tests to be run.
 require('./reservation/getAllForRestaurant');
 
 // MUST BE LAST TO CLOSE THE DATABASE CONNECTION
