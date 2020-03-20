72a5bcc902e7212130cef18ab3aa3edd8c6e43b9
Jacob Allen <35180619+jacoballen4534@users.noreply.github.com>
2020-03-20 23:20:00 +1300

    Setup Backend CI Pipeline (#173)
    * Add GH actions pipeline yml
    
    * Add github actions environment variable to decide connection
    
    * Add a delay to GH actions tests as this replaces the usual sleep command
    
    * Add GH actions pipeline yml
    
    * Add github actions environment variable to decide connection
    
    * Add a delay to GH actions tests as this replaces the usual sleep command

--- backend/tests/tests.js
+++ backend/tests/tests.js
@@ -1,9 +1,15 @@
 import config from '../config';
+import dotenv from 'dotenv';
 
-if (!config.mock) {
+dotenv.config();
+
+if (!process.env.GH_ACTIONS && !config.mock) {
   throw '\n\n   Tests should only be run on the mock database. Please set the mock flag in the config to true.\n \
   If you know what you are doing and wish to run this on a real database, remove this check.\n\n';
 }
+
+// MUST BE FIRST
+require('./setup');
 
 // List all tests to be run.
 require('./reservation/getAllForRestaurant'); // Using IDs 1-5
