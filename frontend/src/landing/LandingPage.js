b7c3876a4696d5d4fcf24d2d26864da5052d8d8a
Bryan <bryanang1998@gmail.com>
2020-04-16 20:21:46 +1200

    Called getRestaurantBookings from Landing page when button is pressed

--- frontend/src/landing/LandingPage.js
+++ frontend/src/landing/LandingPage.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { useHistory } from "react-router";
 import Button from "@material-ui/core/Button";
+import { connect } from "react-redux";
 import style from "./LandingPage.module.css";
 import BookingEditPopupButton from "./edit/BookingEditPopupButton";
 import RestaurantTile from "./RestaurantTile";
 import changePath from "../general/helperFunctions";
 import SearchBar from "./SearchBar";
 import { ReactComponent as Logo2 } from "../general/Logo2.svg";
+import { getRestaurantBookings } from "../store/booking/bookingActions";
+
 
 const LandingPage = (props) => {
-  const { setRestaurant } = props;
+  const { setRestaurant, onRestaurantClicked } = props;
   const history = useHistory();
+  const restaurantID = 1;
 
   const toBooking = () => {
     changePath("/booking", history);
+  };
+
+  const toRestaurants = () => {
+    onRestaurantClicked(restaurantID);
+    changePath("/restaurant", history);
   };
 
   return (
@@ -32,7 +41,7 @@ const LandingPage = (props) => {
               <Button
                 variant="outlined"
                 className={style.secondaryButton}
-                onClick={() => changePath("/restaurant", history)}
+                onClick={() => toRestaurants()}
               >
                 {/* Todo replace with something else */}
                 RESTAURANT
@@ -48,4 +57,9 @@ const LandingPage = (props) => {
   );
 };
 
-export default LandingPage;
+
+const mapDispatchToProps = (dispatch) => ({
+  onRestaurantClicked: (restaurantID) => { dispatch(getRestaurantBookings(restaurantID)); },
+});
+
+export default connect(null, mapDispatchToProps)(LandingPage);
