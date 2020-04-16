cc4346975df57f07b30fc7a635cded1071e77782
Hongye Shi <hongye@1007@gmail.com>
2020-04-16 14:40:44 +1200

    added reviews file and get request

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -10,23 +10,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 /**
  * @swagger
  *
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- * /reviews/{reviewID}:
-=======
  * /restaurant:
->>>>>>> 181f028... added reviews file and get request
-=======
- * /reviews:
->>>>>>> fb8e382... added review post method
-=======
- * /reviews:
->>>>>>> 304a074... added review post method
-=======
- * /reviews/{reviewID}:
->>>>>>> 09e60cd... added swagger documentation
  *   get:
  *     description: Fetch a review object
  *     produces:
@@ -57,272 +41,30 @@ router.get('/:reviewID', async (req, res) => {
     res.json(results);
   });
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 /**
  * @swagger
  *
- * /reviews/restaurant/{restaurantID}:
-=======
-=======
->>>>>>> 304a074... added review post method
-/**
- * @swagger
- *
-<<<<<<< HEAD
- * /reviews:
-<<<<<<< HEAD
->>>>>>> fb8e382... added review post method
-=======
->>>>>>> 304a074... added review post method
-=======
- * /reviews/restaurant/{restaurantID}:
->>>>>>> 09e60cd... added swagger documentation
- *   get:
- *     description: Fetch a list of review objects
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: restaurantID
- *         description: Primary Key of restaurant database table
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: Returns a lsit of review objects for the specified restaurant ID
- */
-router.get('/restaurant/:restaurantID', async (req, res) => {
-  const { restaurantID } = req.params;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  if (!restaurantID) {
-    res.status(400).json({ error: 'GET reviews/restaurant/{id} invocation error: {id} must be an int' });
-    return;
-  }
-
-=======
-=======
->>>>>>> 304a074... added review post method
-
-  if (!restaurantID) {
-    res.status(400).json({ error: 'GET reviews/restaurant/{id} invocation error: {id} must be an int' });
-    return;
-  }
-<<<<<<< HEAD
-
->>>>>>> fb8e382... added review post method
-  connection.query('SELECT * FROM REVIEW WHERE RESTAURANTID = ? ', [restaurantID], (error, results) => {
-    if (error) {
-      res.status(400).json({ error });
-      return;
-    }
-    res.json(results);
-  });
-});
-/**
- * @swagger
- *
- * /reviews:
- *   post:
- *     description: Adds a reviews object to the database
-<<<<<<< HEAD
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: name
- *         description: Name of reviewer
- *         in: formData
- *         required: true
- *         type: string
- *       - name: restaurantID
- *         description: ID of the restaurant the review is for
- *         in: formData
- *         required: true
- *         type: integer
- *       - name: review
- *         description: review description
- *         in: formData
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: Successfully added review to database
- */
-router.post('/', (req, res) => {
-  const { body } = req;
-
-  if (!body.review) {
-    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { review }' });
-    return;
-  }
-
-  if (!body.name) {
-    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { name }' });
-    return;
-  }
-  if (!body.restaurantID) {
-    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { restaurantID }' });
-    return;
-  }
-
-  connection.query(
-    'INSERT INTO REVIEW (`Review`, `Name`, `RestaurantID`) VALUES (?, ?, ?);',
-    [body.review, body.name, body.restaurantID],
-    (error) => {
-      if (error) {
-        res.status(400).json({ error });
-        return;
-      }
-      res.json('added');
-    }
-  );
-});
-=======
->>>>>>> 181f028... added reviews file and get request
-=======
->>>>>>> 304a074... added review post method
-
-  connection.query('SELECT * FROM REVIEW WHERE RESTAURANTID = ? ', [restaurantID], (error, results) => {
-    if (error) {
-      res.status(400).json({ error });
-      return;
-    }
-    res.json(results);
-  });
-});
-/**
- * @swagger
- *
-<<<<<<< HEAD
-<<<<<<< HEAD
- * /reviews:
- *   delete:
- *     description: Deletes a reviews object to the database
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: reviewID
- *         description: Primary Key of reviews database table
-=======
  * /restaurant:
  *   delete:
  *     description: Deletes a restaurant object to the database
-=======
->>>>>>> fb8e382... added review post method
-=======
- * /reviews:
- *   post:
- *     description: Adds a reviews object to the database
->>>>>>> 304a074... added review post method
  *     produces:
  *       - application/json
  *     parameters:
- *       - name: name
- *         description: Name of reviewer
- *         in: formData
- *         required: true
- *         type: string
  *       - name: restaurantID
-<<<<<<< HEAD
-<<<<<<< HEAD
  *         description: Primary Key of Restaurant database table
->>>>>>> 181f028... added reviews file and get request
-=======
-=======
->>>>>>> 304a074... added review post method
- *         description: ID of the restaurant the review is for
- *         in: formData
- *         required: true
- *         type: integer
- *       - name: review
- *         description: review description
- *         in: formData
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: Successfully added review to database
- */
-router.post('/', (req, res) => {
-  const { body } = req;
-
-  if (!body.review) {
-    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { review }' });
-    return;
-  }
-
-  if (!body.name) {
-    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { name }' });
-    return;
-  }
-  if (!body.restaurantID) {
-    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { restaurantID }' });
-    return;
-  }
-
-  connection.query(
-    'INSERT INTO REVIEW (`Review`, `Name`, `RestaurantID`) VALUES (?, ?, ?);',
-    [body.review, body.name, body.restaurantID],
-    (error) => {
-      if (error) {
-        res.status(400).json({ error });
-        return;
-      }
-      res.json('added');
-    }
-  );
-});
-
-/**
- * @swagger
- *
- * /reviews:
- *   delete:
- *     description: Deletes a reviews object to the database
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: reviewID
- *         description: Primary Key of reviews database table
-<<<<<<< HEAD
->>>>>>> fb8e382... added review post method
-=======
->>>>>>> 304a074... added review post method
  *         in: path
  *         required: true
  *         type: integer
  *     responses:
  *       200:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- *         description: Successfully deleted reviews to database
-=======
  *         description: Successfully deleted restaurant to database
->>>>>>> 181f028... added reviews file and get request
-=======
- *         description: Successfully deleted reviews to database
->>>>>>> fb8e382... added review post method
-=======
- *         description: Successfully deleted reviews to database
->>>>>>> 304a074... added review post method
  */
 router.delete('/:reviewID', (req, res) => {
   const { reviewID } = req.params;
 
   if (!reviewID) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    res.status(400).json({ error: 'DELETE /reviews/{id} invocation error: {id} must be an int' });
-=======
     res.status(400).json({ error: 'DELETE /review/{id} invocation error: {id} must be an int' });
->>>>>>> 181f028... added reviews file and get request
-=======
-    res.status(400).json({ error: 'DELETE /reviews/{id} invocation error: {id} must be an int' });
->>>>>>> 09e60cd... added swagger documentation
     return;
   }
 
