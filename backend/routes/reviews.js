304a074e74a51504eada5328c4274fa66ce0fa9b
Hongye Shi <hongye@1007@gmail.com>
2020-04-16 15:08:42 +1200

    added review post method

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -10,7 +10,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 /**
  * @swagger
  *
- * /restaurant:
+ * /reviews:
  *   get:
  *     description: Fetch a review object
  *     produces:
@@ -41,24 +41,110 @@ router.get('/:reviewID', async (req, res) => {
     res.json(results);
   });
 });
-
 /**
  * @swagger
  *
- * /restaurant:
- *   delete:
- *     description: Deletes a restaurant object to the database
+ * /reviews:
+ *   get:
+ *     description: Fetch a list of review objects
  *     produces:
  *       - application/json
  *     parameters:
  *       - name: restaurantID
- *         description: Primary Key of Restaurant database table
+ *         description: Primary Key of restaurant database table
  *         in: path
  *         required: true
  *         type: integer
  *     responses:
  *       200:
- *         description: Successfully deleted restaurant to database
+ *         description: Returns a lsit of review objects for the specified restaurant ID
+ */
+router.get('/restaurant/:restaurantID', async (req, res) => {
+  const { restaurantID } = req.params;
+
+  if (!restaurantID) {
+    res.status(400).json({ error: 'GET reviews/restaurant/{id} invocation error: {id} must be an int' });
+    return;
+  }
+
+  connection.query('SELECT * FROM REVIEW WHERE RESTAURANTID = ? ', [restaurantID], (error, results) => {
+    if (error) {
+      res.status(400).json({ error });
+      return;
+    }
+    res.json(results);
+  });
+});
+/**
+ * @swagger
+ *
+ * /reviews:
+ *   post:
+ *     description: Adds a reviews object to the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: name
+ *         description: Name of reviewer
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: restaurantID
+ *         description: ID of the restaurant the review is for
+ *         in: formData
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully added restaurant to database
+ */
+router.post('/', (req, res) => {
+  const { body } = req;
+
+  if (!body.review) {
+    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { review }' });
+    return;
+  }
+
+  if (!body.name) {
+    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { name }' });
+    return;
+  }
+  if (!body.restaurantID) {
+    res.status(400).json({ error: 'POST reviews/ invocation error: post body needs { restaurantID }' });
+    return;
+  }
+
+  connection.query(
+    'INSERT INTO REVIEW (`Review`, `Name`, `RestaurantID`) VALUES (?, ?, ?);',
+    [body.review, body.name, body.restaurantID],
+    (error) => {
+      if (error) {
+        res.status(400).json({ error });
+        return;
+      }
+      res.json('added');
+    }
+  );
+});
+
+/**
+ * @swagger
+ *
+ * /reviews:
+ *   delete:
+ *     description: Deletes a reviews object to the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: reviewID
+ *         description: Primary Key of reviews database table
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted reviews to database
  */
 router.delete('/:reviewID', (req, res) => {
   const { reviewID } = req.params;
