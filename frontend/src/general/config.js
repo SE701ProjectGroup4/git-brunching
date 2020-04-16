03d987afa0defe404bc67397a48066910f5c1620
Bryan <bryanang1998@gmail.com>
2020-04-16 20:19:32 +1200

    Added endpoint for restaurant bookings to config

--- frontend/src/general/config.js
+++ frontend/src/general/config.js
@@ -3,6 +3,7 @@ const HOST = "http://localhost:3001";
 const GET_ALL_RESTAURANTS = `${HOST}/restaurant/`;
 const RESERVATION = `${HOST}/reservation/`;
 const USER = `${HOST}/user/`;
+const RESTAURANT_BOOKING = (restaurantID) => `${HOST}/reservation?restaurantID=${restaurantID}`;
 const RESTAURANT_HOURS = (restaurantID) => `${HOST}/restaurant/${restaurantID}/openhours`;
 const FREE_TABLE = `${HOST}/table/free`;
 
@@ -11,6 +12,7 @@ export {
   HOST,
   RESERVATION,
   USER,
+  RESTAURANT_BOOKING,
   RESTAURANT_HOURS,
   FREE_TABLE,
 };
