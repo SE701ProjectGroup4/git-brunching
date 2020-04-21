eed0af5a830125d9586f8f262a1627ddaa1f7f55
Gargi Bansal <gban550@aucklanduni.ac.nz>
2020-04-21 16:00:08 +1200

    Removed redundant useEffect import statement

--- frontend/src/landing/SearchBar.js
+++ frontend/src/landing/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { InputAdornment } from "@material-ui/core";
