5b051164e1397fa1bc0633934b188042421be5ac
Raghav Garg <raghavgarg32@gmail.com>
2020-04-22 21:43:23 +1200

    Added a book button to menu popup

--- frontend/src/landing/menu/MenuPopupButton.js
+++ frontend/src/landing/menu/MenuPopupButton.js
@@ -40,6 +40,7 @@ import Button from "@material-ui/core/Button";
             </DialogTitle>
             <div className={style.menuButtonsContainer}>
                 <Button variant="outlined" className={style.primaryButton} onClick={handleClose}>Cancel</Button>
+                <Button variant="outlined" className={style.primaryButton} onClick={handleClose}>Book</Button>
             </div>
         </Dialog>
       </>
